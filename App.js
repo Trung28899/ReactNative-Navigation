9a3f3ea934761a5368278ba484878b1d3644210b
Trung28899 <trung28899@gmail.com>
2021-04-01 15:44:10 -0400

    2nd Commit

--- App.js
+++ App.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
-  return <View>New View Here</View>;
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
+  return (
+    <View>
+      <Text>New View Here</Text>
+    </View>
+  );
 }
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## I. Core Knowledge:
 
 ## II. Module Notes:
+
+    - 2nd Commit: Adding Screens and Fonts:
+        +, AppLoading in App.js is for Splash Screen
+
+## III. Packages:
+
+- \$ npm install --save expo-font
+- \$ npm install --save expo-app-loading
